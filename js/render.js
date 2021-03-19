635b393ab68c7fedf73e503ec2263fa847dfd7e7
Jorge Diz Pico <xurxo@xurxodiz.eu>
2021-03-19 22:43:59 +0100

    Iterates on interface for new bet features

--- js/render.js
+++ js/render.js
@@ -12,10 +12,16 @@ function render(what) {
         .then((response) => response.text())
         .then((template) => {
             let gameToRender = {
-                canReroll: game.remainingRerolls > 0 && game.step == Steps.ROLLING,
                 screen: game.step,
-                betsAreSet: game.step == Steps.BETS, // TODO: Only when bets are final
-                diceNotAcknowledgedYet: game.step == Steps.ROLLING
+                stepSplash: game.step == Steps.SPLASH,
+                stepBets: game.step == Steps.BETS,
+                stepRolling: game.step == Steps.ROLLING,
+                canReroll: game.remainingRerolls > 0,
+                isFirstBet: game.lastAction == LastAction.NONE_YET,
+                currentBet: getCurrentBetText(game.lastAction),
+                betsAreSet: game.betsAreSet,
+                explanationText: getExplationText(game.step, game.subgame),
+                raiseText: getRaiseText(game.lastAction)
             };
             gameToRender.playerDice = game.playerDice.map(function(x, i) {
                 let selected = game.selectedDiceKeys.has(i);
@@ -25,3 +31,36 @@ function render(what) {
             $('#canvas').html(rendered);
         });
 };
+
+function getCurrentBetText(lastAction) {
+    switch (lastAction) {
+        case LastAction.NONE_YET: return "tócache apostar";
+        case LastAction.USER_PASS: return "pasaches, levou " + game.lastBetStanding;
+        case LastAction.RIVAL_PASS: return "pasou, levaches " + game.lastBetStanding;
+        case LastAction.USER_RAISE: return "subiches a " + game.lastBetStanding;
+        case LastAction.RIVAL_RAISE: return "subiuche a " + game.lastBetStanding;
+        case LastAction.USER_CHECK: return "quedan a ver " + game.lastBetStanding;
+        case LastAction.RIVAL_CHECK: return "quedan a ver " + game.lastBetStanding;
+        case LastAction.USER_FOLD: return "pasaches, levou " + game.lastBetStanding;
+        case LastAction.RIVAL_FOLD: return "pasou, levaches " + game.lastBetStanding;
+    }
+}
+
+function getRaiseText(lastAction) {
+    return (lastAction == LastAction.NONE_YET)? "Apostar" : "Subir";
+}
+
+function getExplationText(step, subgame) {
+    switch (step) {
+        case Steps.SPLASH: return "Dámosche a benvida a DECIMUS, o xogo de dados retromedieval!";
+        case Steps.ROLLING: return "Tes dúas oportunidades de rolar de novo tantos dados como queiras.";
+        case Steps.BETS:
+            switch (subgame) {
+                case Subgames.BIGGEST: return "En MAIORES contan os dados con maior valor.";
+                case Subgames.SMALLEST: return "En MENORES contan os dados con menor valor.";
+                case Subgames.ONE_AS_TWO: return "En TANTO EN UN COMO EN DOUS conta ter os dados máis pequenos tal que dous sumen o do outro.";
+                case Subgames.PAIR_PLUS_ACE: return "En PAR E AS conta a maior parella acompañada de as que teñas.";
+            }
+        case Steps.RESULTS: return "Velaquí o resultado das túas apostas.";
+    }
+}
